902736864716db602be5239eac2b517e8e2e5b16
Sai, Yilin (Data61, Eveleigh) <Yilin.Sai@data61.csiro.au>
2024-03-19 13:15:39 +1100

    add test result

--- perftest/testScript.mjs
+++ perftest/testScript.mjs
@@ -3,19 +3,20 @@ import nJwt from "njwt";
 import secureRandom from "secure-random";
 import Chance from "chance";
 import _ from "lodash";
+import http from "node:http";
 
 const chance = new Chance();
 
 /** need to update after Blockchain restart */
 const namespace =
-  "1220d830f781e3cb095dfbbcca64f30347d45af2f424507b342a23496915e7e2b410";
+  "1220d57c61aa64c50c97a284475969e3cdf4fa5554137debb1cd73dc80cea13c2df3";
 
 /** test dimensions */
 const n = 10;
-const d = 10;
+const d = 100;
 const l = 10;
 const m = 1;
-const t = 10;
+const t = 30;
 
 /** this is a user who can act as any party */
 const claims = {
@@ -26,8 +27,9 @@ const signingKey = secureRandom(256, { type: "Buffer" });
 const token = nJwt.create(claims, signingKey).compact();
 const api = axios.create({
   baseURL: "http://localhost:7575",
-  //   timeout: 20000,
+  timeout: 30000,
   headers: { Authorization: `Bearer ${token}` },
+  httpAgent: new http.Agent({ keepAlive: true }),
 });
 
 async function setup() {
@@ -48,10 +50,7 @@ async function setup() {
     parties.map((modelOwner) => [
       modelOwner,
       [...Array(l).keys()].map((i) => ({
-        id: `L:${chance.string({
-          alpha: true,
-          length: 10,
-        })}`,
+        id: `L:${chance.guid()}`,
         scope: "scope",
         copyrightOwnerId: "cro",
         modelOwner,
@@ -65,10 +64,7 @@ async function setup() {
     parties.map((modelOwner) => [
       modelOwner,
       [...Array(d).keys()].map((i) => {
-        const dsId = `DS:${chance.string({
-          alpha: true,
-          length: 10,
-        })}`;
+        const dsId = `DS:${chance.guid()}`;
         const lIndex = chance.pickone([...Array(l).keys()]);
         licenses[modelOwner][lIndex].datasetList.push(dsId);
         const lId = licenses[modelOwner][lIndex].id;
@@ -89,7 +85,7 @@ async function setup() {
       modelOwner,
       [...Array(m).keys()].map((i) => {
         const mId = `M:${modelOwner}:${i}`;
-        const dsIndexes = chance.pickset([...Array(l).keys()], t);
+        const dsIndexes = chance.pickset([...Array(d).keys()], t);
         dsIndexes.forEach((dsIndex) => {
           datasets[modelOwner][dsIndex].modelList.push(mId);
         });
@@ -110,16 +106,16 @@ async function setup() {
 
   async function writeDS() {
     const ds = _.flatten(_.values(datasets));
-    await Promise.all(
-      ds.map(async (dataset) => {
-        await api
-          .post("/v1/create", {
-            templateId: "CRM:DatasetMeta",
-            payload: dataset,
-          })
-          .catch((e) => console.log(e.response.data));
-      })
-    );
+    for (const dataset of ds) {
+      await api
+        .post("/v1/create", {
+          templateId: "CRM:DatasetMeta",
+          payload: dataset,
+        })
+        .catch((e) => {
+          console.log(JSON.stringify(e.message));
+        });
+    }
   }
 
   /** datasets */
@@ -127,16 +123,14 @@ async function setup() {
 
   async function writeL() {
     const ls = _.flatten(_.values(licenses));
-    await Promise.all(
-      ls.map(async (license) => {
-        await api
-          .post("/v1/create", {
-            templateId: "CRM:License",
-            payload: license,
-          })
-          .catch((e) => console.log(e.response.data));
-      })
-    );
+    for (const license of ls) {
+      await api
+        .post("/v1/create", {
+          templateId: "CRM:License",
+          payload: license,
+        })
+        .catch((e) => console.log(e.message));
+    }
   }
 
   /** licenses */
@@ -144,16 +138,14 @@ async function setup() {
 
   async function writeM() {
     const ms = _.flatten(_.values(models));
-    await Promise.all(
-      ms.map(async (model) => {
-        await api
-          .post("/v1/create", {
-            templateId: "CRM:ModelMeta",
-            payload: model,
-          })
-          .catch((e) => console.log(e.response.data));
-      })
-    );
+    for (const model of ms) {
+      await api
+        .post("/v1/create", {
+          templateId: "CRM:ModelMeta",
+          payload: model,
+        })
+        .catch((e) => console.log(e.message));
+    }
   }
 
   /** models */
@@ -163,13 +155,17 @@ async function setup() {
   // console.log(JSON.stringify(datasets, null, 2));
   // console.log(JSON.stringify(models, null, 2));
   return {
-    models: _.flatten(_.values(models)),
-    datasets: _.flatten(_.values(datasets)),
+    models: _.values(models),
     licenses: _.flatten(_.values(licenses)),
   };
 }
 
 async function getModelLicenses(modelId, modelOwner) {
+  // base case
+  if (!modelId) {
+    return;
+  }
+
   // get model meta
   const model = await api
     .post("/v1/query", {
@@ -180,38 +176,58 @@ async function getModelLicenses(modelId, modelOwner) {
       readers: [modelOwner],
     })
     .then((res) => res.data.result[0].payload)
-    .catch((e) => console.log(e.response.data));
+    .catch((e) => console.log(e.message));
 
   // get dataset meta
-  const datasets = await Promise.all(
-    model.datasetList.map(async (dId) => {
-      return await api
-        .post("/v1/query", {
-          templateIds: ["CRM:DatasetMeta"],
-          query: {
-            id: dId,
-          },
-          readers: [modelOwner],
-        })
-        .then((res) => res.data.result[0].payload)
-        .catch((e) => console.log(e.response.data));
-    })
-  );
+  const datasets = [];
+  for (const dId of model.datasetList) {
+    const d = await api
+      .post("/v1/query", {
+        templateIds: ["CRM:DatasetMeta"],
+        query: {
+          id: dId,
+        },
+        readers: [modelOwner],
+      })
+      .then((res) => res.data.result[0].payload)
+      .catch((e) => console.log(e.message));
+    datasets.push(d);
+  }
 
   // get license
-  await Promise.all(
-    datasets.map(async (d) => {
-      return await api
-        .post("/v1/query", {
-          templateIds: ["CRM:License"],
-          query: {
-            id: d.licenseId,
-          },
-          readers: [modelOwner],
-        })
-        .then((res) => res.data.result[0].payload)
-        .catch((e) => console.log(e.response.data));
+  for (const d of datasets) {
+    await api
+      .post("/v1/query", {
+        templateIds: ["CRM:License"],
+        query: {
+          id: d.licenseId,
+        },
+        readers: [modelOwner],
+      })
+      .then((res) => res.data.result[0].payload)
+      .catch((e) => console.log(e.message));
+  }
+
+  // recursively on source model
+  await getModelLicenses(model.sourceModel, modelOwner);
+}
+
+async function getDownstreamModels(modelId, modelOwner) {
+  // get model meta
+  const model = await api
+    .post("/v1/query", {
+      templateIds: ["CRM:ModelMeta"],
+      query: {
+        id: modelId,
+      },
+      readers: [modelOwner],
     })
+    .then((res) => res.data.result[0].payload)
+    .catch((e) => console.log(e.message));
+
+  // recursively on child models
+  await Promise.all(
+    model.childModels.map((mId) => getDownstreamModels(mId, modelOwner))
   );
 }
 
@@ -226,7 +242,7 @@ async function getModelsByLicense(licenseId, modelOwner) {
       readers: [modelOwner],
     })
     .then((res) => res.data.result[0].payload)
-    .catch((e) => console.log(e.response.data));
+    .catch((e) => console.log(e.message));
 
   // get dataset meta
   const datasets = await Promise.all(
@@ -240,16 +256,16 @@ async function getModelsByLicense(licenseId, modelOwner) {
           readers: [modelOwner],
         })
         .then((res) => res.data.result[0].payload)
-        .catch((e) => console.log(e.response.data));
+        .catch((e) => console.log(e.message));
     })
   );
 
   // get model meta
-  const models = await Promise.all(
+  await Promise.all(
     datasets.map(async (d) => {
       return await Promise.all(
-        d.modelList.map((m) =>
-          api
+        d.modelList.map(async (m) => {
+          await api
             .post("/v1/query", {
               templateIds: ["CRM:ModelMeta"],
               query: {
@@ -258,14 +274,20 @@ async function getModelsByLicense(licenseId, modelOwner) {
               readers: [modelOwner],
             })
             .then((res) => res.data.result[0].payload)
-            .catch((e) => console.log(e.response.data))
-        )
+            .catch((e) => console.log(e.message));
+          await getDownstreamModels(m, modelOwner);
+        })
       );
     })
   );
 }
 
 async function getModelDatasets(modelId, modelOwner) {
+  // base case
+  if (!modelId) {
+    return;
+  }
+
   // get model meta
   const model = await api
     .post("/v1/query", {
@@ -276,29 +298,39 @@ async function getModelDatasets(modelId, modelOwner) {
       readers: [modelOwner],
     })
     .then((res) => res.data.result[0].payload)
-    .catch((e) => console.log(e.response.data));
+    .catch((e) => console.log(e.message));
 
   // get dataset meta
-  const datasets = await Promise.all(
-    model.datasetList.map(async (dId) => {
-      return await api
-        .post("/v1/query", {
-          templateIds: ["CRM:DatasetMeta"],
-          query: {
-            id: dId,
-          },
-          readers: [modelOwner],
-        })
-        .then((res) => res.data.result[0].payload)
-        .catch((e) => console.log(e.response.data));
-    })
-  );
+  for (const dId of model.datasetList) {
+    await api
+      .post("/v1/query", {
+        templateIds: ["CRM:DatasetMeta"],
+        query: {
+          id: dId,
+        },
+        readers: [modelOwner],
+      })
+      .then((res) => res.data.result[0].payload)
+      .catch((e) => console.log(e.message));
+  }
+
+  // recursively on source model
+  await getModelDatasets(model.sourceModel, modelOwner);
+}
+
+function getStandardDeviation(array) {
+  const n = array.length;
+  const mean = array.reduce((a, b) => a + b) / n;
+  return Math.sqrt(
+    array.map((x) => Math.pow(x - mean, 2)).reduce((a, b) => a + b) / n
+  ).toFixed(1);
 }
 
 async function main() {
   const { models, licenses } = await setup();
 
-  const testModels = chance.pickset(models, 10);
+  const testParties = chance.pickset([...Array(n).keys()], 10);
+  const testModels = testParties.map((index) => models[index][m - 1]);
   const time1 = await Promise.all(
     testModels.map(async (m) => {
       var start = new Date();
@@ -308,7 +340,8 @@ async function main() {
     })
   );
   const average1 = _.mean(time1);
-  console.log("getModelLicenses:", average1);
+  const std1 = getStandardDeviation(time1);
+  console.log("getModelLicenses:", average1, std1);
 
   const testLicenses = chance.pickset(licenses, 10);
   const time2 = await Promise.all(
@@ -320,7 +353,8 @@ async function main() {
     })
   );
   const average2 = _.mean(time2);
-  console.log("getModelsByLicense", average2);
+  const std2 = getStandardDeviation(time2);
+  console.log("getModelsByLicense:", average2, std2);
 
   const time3 = await Promise.all(
     testModels.map(async (m) => {
@@ -331,7 +365,8 @@ async function main() {
     })
   );
   const average3 = _.mean(time3);
-  console.log("getModelDatasets", average3);
+  const std3 = getStandardDeviation(time3);
+  console.log("getModelDatasets:", average3, std3);
 }
 
 main();
